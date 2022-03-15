21883d65508cda5b5ade548618fed32b33818e1c
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-03-16 08:05:54 +0900

    Create "Sake Nenkaku" component

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <p :style="alcoholicDrinkMsgStyle">age: {{ age }} / {{  alcoholicDrinkMsg }}</p>
+  <div>
+    <button @click="incrementAge">+1 age</button>
+    <button @click="decrementAge">-1 age</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      age: 20,
+    }
+  },
+  computed: {
+    canGetAlcoholicDrink(){
+      return this.age >= 20
+    },
+    alcoholicDrinkMsg(){
+      return this.canGetAlcoholicDrink ? "You can get alcoholic drink" : "You can't get alcoholic drink yet"
+    },
+    
+    alcoholicDrinkMsgStyle(){
+      return this.canGetAlcoholicDrink ? { color: "green", } : { color: "red", textTransform: "uppercase"}
+    }
+  },
+  methods: {
+    incrementAge(){
+      this.age++
+    },
+    decrementAge(){
+      this.age--
+    }
+  }
+}
+</script>
