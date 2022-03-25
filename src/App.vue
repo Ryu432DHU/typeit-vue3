a97e1c64a2a4252a8c00da2c617533142c5e49b6
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-03-25 20:48:35 +0900

    Make code beutiful as Ron advised me

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,15 @@ import MyFirstComponent from './components/atoms/MyFirstComponent.vue';
 </script>
 
 <template>
-  <MyFirstComponent>
-    <template v-slot:header>
-      <p>Here might be a page title</p>
-    </template>
-    <template v-slot:footer>
-      <p>Here's some contact info</p>
-    </template>
-    <template v-slot:default><h1>Default slot</h1></template>
-  </MyFirstComponent>
+  <div class="container mx-auto">
+    <MyFirstComponent alcoholicDrinkName="コーラ">
+      <template v-slot:header>
+        <p>Here might be a page title</p>
+      </template>
+      <template v-slot:footer>
+        <p>Here's some contact info</p>
+      </template>
+      <template v-slot:default><h1>Default slot</h1></template>
+    </MyFirstComponent>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-.hoge {
-  color: red;
-}
-</style>
--- src/components/atoms/MyFirstComponent.vue
+++ src/components/atoms/MyFirstComponent.vue
@@ -1,4 +1,5 @@
 <template>
+  <p>Drink name:{{ alcoholicDrinkName }}</p>
   <p :style="alcoholicDrinkMsgStyle">age: {{ age }} / {{  alcoholicDrinkMsg }}</p>
   <p>{{ canGetAlcoholicDrink }}</p>
   <div>
@@ -6,20 +7,23 @@
     <button class="border-2 border-gray-500 px-4 py-2 rounded-md" @click="decrementAge">-1 age</button>
   </div>
   <slot name="header" />
+  <slot name="default" />
   <slot>Fallback content</slot>
   <slot name="footer" />
-  <slot name="default" />
 </template>
 
 <script setup>
 import { computed, ref } from 'vue'
 
+defineProps({
+  alcoholicDrinkName: String
+})
+
 const age = ref(20)
 const canGetAlcoholicDrink = computed(() => age.value >= 20)
 const alcoholicDrinkMsg = computed(() => {
-  return canGetAlcoholicDrink.value
-    ? "You can get alchoholic drink"
-    : "You can not get alchoholic drink yet"
+  const possibility = canGetAlcoholicDrink.value ? "can" : "can not"
+  return `You ${possibility} get alcoholic drink`
 })
 const alcoholicDrinkMsgStyle = computed(() => {
   return { color: canGetAlcoholicDrink.value ? "green" : "red"}
