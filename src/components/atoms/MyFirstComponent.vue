68481de31b801355cf475cdfd32cbcdbafbe7e78
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-03-25 19:53:55 +0900

    Rewrite Options API to Composition API

--- src/components/atoms/MyFirstComponent.vue
+++ src/components/atoms/MyFirstComponent.vue
@@ -1,37 +1,27 @@
 <template>
   <p :style="alcoholicDrinkMsgStyle">age: {{ age }} / {{  alcoholicDrinkMsg }}</p>
+  <p>{{ canGetAlcoholicDrink }}</p>
   <div>
     <button class="border-2 border-gray-500 mr-4 px-4 py-2 rounded-md" @click="incrementAge">+1 age</button>
     <button class="border-2 border-gray-500 px-4 py-2 rounded-md" @click="decrementAge">-1 age</button>
   </div>
 </template>
 
-<script>
-export default {
-  data(){
-    return {
-      age: 20,
-    }
-  },
-  computed: {
-    canGetAlcoholicDrink(){
-      return this.age >= 20
-    },
-    alcoholicDrinkMsg(){
-      return this.canGetAlcoholicDrink ? "You can get alcoholic drink" : "You can't get alcoholic drink yet"
-    },
-    
-    alcoholicDrinkMsgStyle(){
-      return this.canGetAlcoholicDrink ? { color: "green", } : { color: "red", textTransform: "uppercase"}
-    }
-  },
-  methods: {
-    incrementAge(){
-      this.age++
-    },
-    decrementAge(){
-      this.age--
-    }
-  }
-}
+<script setup>
+import { computed, ref } from 'vue'
+
+const age = ref(20)
+const canGetAlcoholicDrink = computed(() => age.value >= 20)
+const alcoholicDrinkMsg = computed(() => {
+  return canGetAlcoholicDrink.value
+    ? "You can get alchoholic drink"
+    : "You can not get alchoholic drink yet"
+})
+const alcoholicDrinkMsgStyle = computed(() => {
+  return { color: canGetAlcoholicDrink.value ? "green" : "red"}
+})
+
+const incrementAge = () => { age.value++ }
+const decrementAge = () => { age.value-- }
+
 </script>
