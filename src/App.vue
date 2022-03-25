bceb754e6a7f434c1b978e2e64b2c643e59db5ce
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-03-25 19:39:00 +0900

    Create folders for Atomic Design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import MyFirstComponent from './components/MyFirstComponent.vue';
+import HelloWorld from './components/atoms/HelloWorld.vue'
+import MyFirstComponent from './components/atoms/MyFirstComponent.vue';
 </script>
 
 <template>
