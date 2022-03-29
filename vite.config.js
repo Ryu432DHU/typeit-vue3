8e622fefb959068d77c97d679244026a728b1d69
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-03-29 21:00:58 +0900

    Test production build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/typeit-vue3/",
+  base: (process.env.NODE_ENV === "production") ? "/typeit-vue3/" : "/",
   build: {
     outDir: "./docs"
   }
