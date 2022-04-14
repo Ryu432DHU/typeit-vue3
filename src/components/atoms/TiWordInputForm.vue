743817c775ba5f051bbdc777878120c2151e1137
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-14 10:26:03 +0900

    Adjust height

--- src/components/atoms/TiWordInputForm.vue
+++ src/components/atoms/TiWordInputForm.vue
@@ -1,7 +1,7 @@
 <template>
   <input type="text"
          placeholder="Type the word above here"
-         class="block w-full outline-none bg-neutral-800 text-white text-center text-2xl p-8"
+         class="block w-full outline-none bg-neutral-800 text-white text-center text-2xl p-10 rounded-none"
          @input="$emit('update:modelValue', $event.target.value)"
          :value="modelValue"
   >
