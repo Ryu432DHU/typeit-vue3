487494ec1ff918a409d545520a70a0de629246c4
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-18 16:43:43 +0900

    Improve button design

--- src/components/atoms/TiButton.vue
+++ src/components/atoms/TiButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="text-sm border-none rounded border-slate-300 px-4 py-1 shadow-md bg-zinc-100 hover:bg-zinc-200 transition-all">
+  <button class="text-sm uppercase font-medium border-none rounded border-slate-300 px-4 py-2 shadow-md bg-zinc-100 hover:bg-zinc-200 transition-all">
     <slot />
   </button>
 </template>
