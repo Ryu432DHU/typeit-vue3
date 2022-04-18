a16fb4e4195159611ba7db5d65376c9baef7ea7e
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-18 16:39:47 +0900

    Add hover style

--- src/components/atoms/TiButton.vue
+++ src/components/atoms/TiButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="text-sm border rounded border-slate-300 drop-shadow-md  px-4 py-1">
+  <button class="text-sm border-none rounded border-slate-300 px-4 py-1 shadow-md bg-zinc-100 hover:bg-zinc-200 transition-all">
     <slot />
   </button>
 </template>
