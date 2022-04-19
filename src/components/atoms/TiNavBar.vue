deba89e6ead1e5797bcf9d3f2f89dec48bd13f5c
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-19 14:59:05 +0900

    Highlight text links properly

--- src/components/atoms/TiNavBar.vue
+++ src/components/atoms/TiNavBar.vue
@@ -5,14 +5,8 @@
         <router-link to="/">TypeIt</router-link>
       </h1>
       <ul class="flex">
-        <li class="text-white text-sm px-4 py-2">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="text-neutral-400 text-sm px-4 py-2">
-          <router-link to="/rankings">Rankings</router-link>
-        </li>
-        <li class="text-neutral-400 text-sm px-4 py-2">
-          <router-link to="/edit">Edit</router-link>
+        <li v-for="(route, index) in routes" :key="route.path" class="text-sm px-4 py-2" :class="linkColors[index]">
+          <router-link :to="route.path">{{ route.text }}</router-link>
         </li>
         <li class="text-neutral-400 text-sm px-4 py-2">
           <a href="https://github.com/Ryu432DHU/typeit-vue3">GitHub</a>
@@ -23,5 +17,27 @@
 </template>
 
 <script setup>
+import { computed, watch } from 'vue';
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const routes = [
+  {
+    path: "/", text: "Home"
+  },
+  {
+    path: "/rankings", text: "Rankings"
+  },
+  {
+    path: "/edit", text: "Edit"
+  },
+]
+
+const linkColors = computed(() => routes.map(route => route.path === router.currentRoute.value.path ? 'text-white' : 'text-neutral-400'))
+
+watch(router.currentRoute, () => {
+  console.log(router.currentRoute.value)
+  console.log(routes.map(route => route.path === router.currentRoute.value.path))
+})
 </script>
