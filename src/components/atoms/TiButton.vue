3ea3cdb35930620cf47ef6090ae4728135e877fe
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-19 15:11:31 +0900

    Support the "disabled" property

--- src/components/atoms/TiButton.vue
+++ src/components/atoms/TiButton.vue
@@ -1,11 +1,13 @@
 <template>
-  <button class="text-sm uppercase font-medium border-none rounded border-slate-300 px-4 py-2 shadow-md bg-zinc-100 hover:bg-zinc-200 transition-all">
+  <button class="text-sm uppercase font-medium border-none rounded border-slate-300 px-4 py-2 shadow-md bg-zinc-100 hover:bg-zinc-200 disabled:bg-zinc-200 disabled:text-zinc-500 disabled:shadow-none transition-all"
+    :disabled="props.disabled"
+  >
     <slot />
   </button>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   disabled: Boolean
 })
 </script>
