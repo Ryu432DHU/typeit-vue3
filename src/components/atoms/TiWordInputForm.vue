13f285bf80df42976a07da14cb08a4303da6ce15
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-20 18:16:03 +0900

    Add :hover state

--- src/components/atoms/TiWordInputForm.vue
+++ src/components/atoms/TiWordInputForm.vue
@@ -1,7 +1,7 @@
 <template>
   <input type="text"
          placeholder="Type the word above here"
-         class="block w-full outline-none bg-neutral-800 text-white text-center text-2xl p-10 rounded-none"
+         class="block w-full outline-none bg-neutral-800 text-white text-center text-2xl p-10 rounded-none focus:bg-neutral-900 transition-all duration-300"
          @input="$emit('update:modelValue', $event.target.value)"
          :value="modelValue"
   >
