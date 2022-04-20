4e25cefb7013ca5770044786d22d6c73c39b7949
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-20 18:04:41 +0900

    Expand link area

--- src/components/atoms/TiNavBar.vue
+++ src/components/atoms/TiNavBar.vue
@@ -1,15 +1,15 @@
 <template>
-  <nav class="bg-neutral-900 py-4">
-    <div class="container max-w-7xl w-10/12 mx-auto flex justify-between">
+  <nav class="bg-neutral-900">
+    <div class="container max-w-7xl w-10/12 mx-auto flex justify-between items-center">
       <h1 class="text-2xl text-white font-light">
         <router-link to="/">TypeIt</router-link>
       </h1>
       <ul class="flex">
-        <li v-for="(route, index) in routes" :key="route.path" class="text-sm px-4 py-2" :class="linkColors[index]">
-          <router-link :to="route.path">{{ route.text }}</router-link>
+        <li v-for="(route, index) in routes" :key="route.path">
+          <router-link :to="route.path" class="inline-block text-sm px-4 py-6" :class="linkColors[index]">{{ route.text }}</router-link>
         </li>
-        <li class="text-neutral-400 text-sm px-4 py-2">
-          <a href="https://github.com/Ryu432DHU/typeit-vue3">GitHub</a>
+        <li>
+          <a href="https://github.com/Ryu432DHU/typeit-vue3" class="text-neutral-400 text-sm inline-block px-4 py-6">GitHub</a>
         </li>
       </ul>
     </div>
