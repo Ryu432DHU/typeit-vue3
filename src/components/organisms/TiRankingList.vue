1ce066dd2fa7db966b50832c552b40f8249164f2
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-22 10:17:36 +0900

    Support the "sort time record" feature

--- src/components/organisms/TiRankingList.vue
+++ src/components/organisms/TiRankingList.vue
@@ -6,7 +6,7 @@
       <th>Time</th>
       <th>Date</th>
     </tr>
-    <tr v-for="(record, index) in props.wordList.records">
+    <tr v-for="(record, index) in sortedRecords">
       <td>{{ index + 1 }}</td>
       <td>{{ wordList.words.length }} words</td>
       <td>{{ record.time }} seconds</td>
@@ -16,9 +16,12 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import TiSimpleTable from '../atoms/TiSimpleTable.vue'
 
-const props = defineProps({
-  wordList: Object
+const props = defineProps({ wordList: Object })
+
+const sortedRecords = computed(() => {
+  return props.wordList.records.sort((a, b) => a.time < b.time ? -1 : 1)
 })
 </script>
