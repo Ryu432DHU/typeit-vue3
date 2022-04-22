c87cc8d462cb5362fcd69095840574e9d4db0a7a
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-04-22 10:18:13 +0900

    Add new mockup data

--- src/store/wordLists.js
+++ src/store/wordLists.js
@@ -2,7 +2,7 @@ const wordLists = [
   {
     name: "example",
     words: ["qw", "er", "ty"],
-    records: [{ date: new Date(), time: 100 }, { date: new Date(), time: 90 }]
+    records: [{ date: new Date(), time: 50 }, { date: new Date(), time: 100 }, { date: new Date(), time: 90 }]
   },
   {
     name: "Rounded",
