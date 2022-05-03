c4674ac55346129cebc66e30989b83c580b0e55f
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-05-03 16:30:12 +0900

    Make text smaller

--- src/components/atoms/TiChip.vue
+++ src/components/atoms/TiChip.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="text-sm bg-neutral-200 rounded-full mr-2 mb-1 px-3 py-2" :class="chipStyle(color)">
+  <span class="text-xs bg-neutral-200 rounded-full mr-2 mb-1 px-3 py-2" :class="chipStyle(color)">
     <slot></slot>
   </span>
 </template>
