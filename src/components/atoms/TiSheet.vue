e1d402281ac39362de738215cf90bd673c064198
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-06-24 08:54:19 +0900

    Expand width

--- src/components/atoms/TiSheet.vue
+++ src/components/atoms/TiSheet.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-neutral-50 max-w-7xl w-10/12 mx-auto mt-12">
+  <div class="bg-neutral-50 mt-12 max-w-7xl w-11/12 sm:w-10/12 mx-auto">
     <slot></slot>
   </div>
 </template>
