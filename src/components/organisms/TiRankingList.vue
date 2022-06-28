82e4409434012178633eb3193da62044f6a67964
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-06-28 21:32:44 +0900

    Show the entire word

--- src/components/organisms/TiRankingList.vue
+++ src/components/organisms/TiRankingList.vue
@@ -8,8 +8,8 @@
     </tr>
     <tr v-for="(record, index) in filteredTop5records">
       <td>{{ index + 1 }}</td>
-      <td class="hidden sm:table-cell">{{ wordList.words.length }} words</td>
-      <td>{{ record.time }} sec<span class="hidden sm:inline">onds</span></td>
+      <td >{{ wordList.words.length }} words</td>
+      <td>{{ record.time }} seconds</td>
       <td>{{ getRecordDateText(record.date) }}</td>
     </tr>
   </ti-simple-table>
