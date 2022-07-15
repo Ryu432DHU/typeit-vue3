5ff4319c2ee3c8e9e959dacfe13e8af3cb586033
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-07-15 23:35:10 +0900

    Add Genshin Impact Characters

--- src/store/wordLists.ts
+++ src/store/wordLists.ts
@@ -2,29 +2,19 @@ import { WordList } from '../types'
 
 const wordLists: WordList[] = [
   {
-    "name": "example",
-    "words": ["qw", "er", "ty"],
-    "records": [
-      {"date": "2022-05-03T08:04:44.705Z", "time":0.8},
-      {"date": "2022-04-30T12:40:17.323Z", "time":0.9},
-      {"date": "2022-04-30T12:40:06.503Z", "time":1.1},
-    ]
+    name: "Mondstadt | Genshin Impact",
+    words: ["Albed", "Amber", "Barbara", "Bennett", "Diluc", "Diona", "Eula", "Fischl", "Jean", "Kaeya", "Klee", "Lisa", "Mona", "Noelle", "Razor", "Rosaria", "Sucrose", "Venti"],
+    records: []
   },
   {
-    "name": "Rounded",
-    "words": ["rounded-none", "rounded-sm", "rounded", "rounded-md", "rounded-lg", "rouded-xl", "rounded-2xl", "rounded-3xl"],
-    "records": [
-      {"date": "2022-05-04T14:38:35.086Z", "time":20.6},
-      {"date": "2022-05-01T02:45:12.234Z", "time":100}
-    ]
+    name: "Liyue | Genshin Impact",
+    words: ["Beidou", "Chongyun", "Ganyu", "Hu Tao", "Keqing", "Ningguang", "Qiqi", "Shenhe", "Xiangling", "Xiao", "Xingqiu", "Xinyan", "Yanfei", "Yelan", "Yun Jin", "Zhongli"],
+    records: []
   },
   {
-    "name": "Colors",
-    "words": ["slate", "gray", "zinc", "neutral", "stone", "red", "orange", "50", "100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    "records": [
-      {"date": "2022-05-04T14:39:02.791Z", "time":23.9},
-      {"date": "2022-05-01T02:45:12.234Z", "time":100}
-    ]
+    name: "Inzazuma | Genshin Impact",
+    words: ["Arataki Itto", "Gorou", "Kaedehara Kazuha", "Kamisato Ayaka", "Kamisato Ayato", "Kujou Sara", "Kuki Shinobu", "Raiden Shogun", "Sangonomiya Kokomi", "Sayu", "Shikanoin Heizou", "Thoma", "Yae Miko", "Yoimiya"],
+    records: []
   }
 ]
 
