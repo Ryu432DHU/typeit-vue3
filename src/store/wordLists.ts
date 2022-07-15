dd528be14490cb89c51ed2903e71cee30804ac81
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-07-15 23:52:03 +0900

    Add Colors and Arknights Characters

--- src/store/wordLists.ts
+++ src/store/wordLists.ts
@@ -2,18 +2,33 @@ import { WordList } from '../types'
 
 const wordLists: WordList[] = [
   {
-    name: "Mondstadt | Genshin Impact",
+    name: "Colors",
+    words: ["Gray", "Red", "Orange", "Amber", "Yellow", "Lime", "Green", "Emerald", "Cyan", "Blue", "Indigo", "Purple", "Pink", "Rose"],
+    records: []
+  },
+  {
+    name: "Genshin Impact | Mondstadt",
     words: ["Albed", "Amber", "Barbara", "Bennett", "Diluc", "Diona", "Eula", "Fischl", "Jean", "Kaeya", "Klee", "Lisa", "Mona", "Noelle", "Razor", "Rosaria", "Sucrose", "Venti"],
     records: []
   },
   {
-    name: "Liyue | Genshin Impact",
+    name: "Genshin Impact | Liyue",
     words: ["Beidou", "Chongyun", "Ganyu", "Hu Tao", "Keqing", "Ningguang", "Qiqi", "Shenhe", "Xiangling", "Xiao", "Xingqiu", "Xinyan", "Yanfei", "Yelan", "Yun Jin", "Zhongli"],
     records: []
   },
   {
-    name: "Inzazuma | Genshin Impact",
+    name: "Genshin Impact | Inzazuma",
     words: ["Arataki Itto", "Gorou", "Kaedehara Kazuha", "Kamisato Ayaka", "Kamisato Ayato", "Kujou Sara", "Kuki Shinobu", "Raiden Shogun", "Sangonomiya Kokomi", "Sayu", "Shikanoin Heizou", "Thoma", "Yae Miko", "Yoimiya"],
+    records: []
+  },
+  {
+    name: "Arknights 1",
+    words: ["Jessica", "Haze", "Ch'en", "Amiya", "Ifrit", "Eyjafjalla", "Vigna", "Blaze", "Savage", "Projekt Red"],
+    records: []
+  },
+  {
+    name: "Arkinghts 2",
+    words: ["Frostleaf", "Vermeli", "Cuora", "Gitano", "Rope", "Exusiai", "Angelina", "Sussurro", "Surtr", "Schwarz"],
     records: []
   }
 ]
