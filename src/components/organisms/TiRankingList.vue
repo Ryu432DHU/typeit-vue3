316affc40b57cc485e3fbcfc0fd537a55b9163a2
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-07-16 20:41:09 +0900

    Show the all time records

--- src/components/organisms/TiRankingList.vue
+++ src/components/organisms/TiRankingList.vue
@@ -6,7 +6,7 @@
       <th>Time</th>
       <th>Date</th>
     </tr>
-    <tr v-for="(record, index) in filteredTop5records">
+    <tr v-for="(record, index) in sortedRecords">
       <td>{{ index + 1 }}</td>
       <td >{{ wordList.words.length }} words</td>
       <td>{{ record.time }} seconds</td>
@@ -22,6 +22,6 @@ import TiSimpleTable from '../atoms/TiSimpleTable.vue'
 
 const props = defineProps<{ wordList: WordList}>()
 const sortedRecords = computed(() =>  props.wordList.records.sort((a, b) => a.time < b.time ? -1 : 1))
-const filteredTop5records = computed(() => sortedRecords.value.slice(0, 5))
+// const filteredTop5records = computed(() => sortedRecords.value.slice(0, 5))
 const getRecordDateText = (date: Date | string): string => new Date(date).toLocaleString()
 </script>
