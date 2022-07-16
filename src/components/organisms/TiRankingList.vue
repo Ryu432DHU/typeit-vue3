e9cc5088e0ae4f1ba95ef289a78e3f15d3e26a1a
Ryu432DHU <a21dc115@dhw.ac.jp>
2022-07-16 20:47:13 +0900

    Show developer's time record on the top

--- src/components/organisms/TiRankingList.vue
+++ src/components/organisms/TiRankingList.vue
@@ -2,9 +2,15 @@
   <ti-simple-table>
     <tr>
       <th>No.</th>
-      <th >Length</th>
+      <th>Length</th>
       <th>Time</th>
       <th>Date</th>
+    </tr>
+    <tr>
+      <td>EX</td>
+      <td >{{ wordList.words.length }} words</td>
+      <td>{{ wordList.records[0].time }} seconds</td>
+      <td>{{ getRecordDateText(wordList.records[0].date) }}</td>
     </tr>
     <tr v-for="(record, index) in sortedRecords">
       <td>{{ index + 1 }}</td>
@@ -21,7 +27,7 @@ import { computed } from 'vue'
 import TiSimpleTable from '../atoms/TiSimpleTable.vue'
 
 const props = defineProps<{ wordList: WordList}>()
-const sortedRecords = computed(() =>  props.wordList.records.sort((a, b) => a.time < b.time ? -1 : 1))
+const sortedRecords = computed(() =>  (props.wordList.records.sort((a, b) => a.time < b.time ? -1 : 1).slice(1)))
 // const filteredTop5records = computed(() => sortedRecords.value.slice(0, 5))
 const getRecordDateText = (date: Date | string): string => new Date(date).toLocaleString()
 </script>
