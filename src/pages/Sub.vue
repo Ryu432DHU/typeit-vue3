e761ef430a17f43bed02cabe11050aca81249916
Ryu432DHU <94089667+Ryu432DHU@users.noreply.github.com>
2022-07-25 06:25:09 +0900

    Expand the width of ti-text-field for mobile

--- src/pages/Sub.vue
+++ src/pages/Sub.vue
@@ -1,7 +1,7 @@
 <template>
   <ti-sheet class="my-4 p-8">
     <h1 class="text-2xl">ti-text-field</h1>
-    <div class="my-4 w-6/12">
+    <div class="my-4 w-8/12">
       <p>Write down your favorite character's name below</p>
       <ti-text-field 
         v-model="text"
